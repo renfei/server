21f594c0e61ef7deae0a4d88e726976e9f83eb19
RenFei <i@renfei.net>
2023-07-28 14:46:01 +0800

    feat: 启用OpenFeign客户端

--- server-main/src/main/java/net/renfei/server/main/Application.java
+++ server-main/src/main/java/net/renfei/server/main/Application.java
@@ -3,12 +3,14 @@ package net.renfei.server.main;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 程序启动入口
  *
  * @author renfei
  */
+@EnableFeignClients(basePackages = {"net.renfei.*"})
 @SpringBootApplication(scanBasePackages = {"net.renfei.**"})
 @MapperScan(basePackages = {"net.renfei.**.repositories"})
 public class Application {
