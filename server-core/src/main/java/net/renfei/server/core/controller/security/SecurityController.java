302c81ea0f7cfb8f37662bdfa4cae505a10fab3d
RenFei <i@renfei.net>
2023-07-28 11:41:20 +0800

    fix: 修复一些错误

--- server-core/src/main/java/net/renfei/server/core/controller/security/SecurityController.java
+++ server-core/src/main/java/net/renfei/server/core/controller/security/SecurityController.java
@@ -32,7 +32,7 @@ public class SecurityController extends BaseController {
     @GetMapping("/core/security/secret-key")
     @Operation(summary = "向服务器申请服务器公钥",
             description = "交换逻辑请查看：https://www.renfei.net/posts/1003346",
-            tags = {"认证鉴权接口"})
+            tags = {"安全接口"})
     @AuditLog(module = "AUTH", operation = "向服务器申请服务器公钥", descriptionExpression = "向服务器申请服务器公钥")
     public ApiResult<SecretKey> requestServerSecretKey() {
         return new ApiResult<>(securityService.requestServerSecretKey());
@@ -47,7 +47,7 @@ public class SecurityController extends BaseController {
     @PostMapping("/core/security/secret-key")
     @Operation(summary = "上报客户端公钥，并下发AES秘钥",
             description = "交换逻辑请查看：https://www.renfei.net/posts/1003346"
-            , tags = {"认证鉴权接口"})
+            , tags = {"安全接口"})
     @AuditLog(module = "AUTH", operation = "上报客户端公钥，并下发AES秘钥", descriptionExpression = "上报客户端公钥，并下发AES秘钥")
     public ApiResult<SecretKey> settingClientSecretKey(@RequestBody SecretKey secretKey) {
         return new ApiResult<>(securityService.settingClientSecretKey(secretKey));
